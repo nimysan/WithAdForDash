201333f72419fd3bb11326602917448a415bf4c1
nimysan <234aini@gmail.com>
2025-01-08 19:36:51 +0800

    update function

--- ad-m4s-function/lib/parse-m4s.js
+++ ad-m4s-function/lib/parse-m4s.js
@@ -1,11 +1,5 @@
-#!/usr/bin/env node
-'use strict';
-
-import fs from 'fs';
-import path from 'path';
-
 /**
- * 解析box的大小和类型
+ * Parse box header
  * @param {Buffer} buffer - 数据buffer
  * @param {number} offset - 起始位置
  * @returns {Object} box信息
@@ -18,10 +12,11 @@ function parseBoxHeader(buffer, offset) {
 }
 
 /**
- * 解析styp box (Segment Type Box)
+ * Parse styp box (Segment Type Box)
  * @param {Buffer} buffer - 数据buffer
  * @param {number} offset - 起始位置
  * @param {number} size - box大小
+ * @returns {Object} styp box信息
  */
 function parseStypBox(buffer, offset, size) {
   const majorBrand = buffer.toString('utf8', offset + 8, offset + 12);
@@ -32,46 +27,46 @@ function parseStypBox(buffer, offset, size) {
     compatibleBrands.push(buffer.toString('utf8', offset + i, offset + i + 4));
   }
   
-  console.log('\n=== Segment Type Box (styp) ===');
-  console.log('Major Brand:', majorBrand);
-  console.log('Minor Version:', minorVersion);
-  console.log('Compatible Brands:', compatibleBrands.join(', '));
+  return {
+    majorBrand,
+    minorVersion,
+    compatibleBrands
+  };
 }
 
 /**
- * 解析sidx box (Segment Index Box)
+ * Parse sidx box (Segment Index Box)
  * @param {Buffer} buffer - 数据buffer
  * @param {number} offset - 起始位置
  * @param {number} size - box大小
+ * @returns {Object} sidx box信息
  */
 function parseSidxBox(buffer, offset, size) {
   const version = buffer.readUInt8(offset + 8);
   const referenceID = buffer.readUInt32BE(offset + 12);
   const timescale = buffer.readUInt32BE(offset + 16);
   
-  console.log('\n=== Segment Index Box (sidx) ===');
-  console.log('Version:', version);
-  console.log('Reference ID:', referenceID);
-  console.log('Timescale:', timescale);
-  
+  let earliestPTS;
   if (version === 0) {
-    const earliestPTS = buffer.readUInt32BE(offset + 20);
-    const firstOffset = buffer.readUInt32BE(offset + 24);
-    console.log('Earliest PTS:', earliestPTS);
-    console.log('First Offset:', firstOffset);
+    earliestPTS = buffer.readUInt32BE(offset + 20);
   } else {
-    const earliestPTS = buffer.readBigUInt64BE(offset + 20);
-    const firstOffset = buffer.readBigUInt64BE(offset + 28);
-    console.log('Earliest PTS:', earliestPTS.toString());
-    console.log('First Offset:', firstOffset.toString());
+    earliestPTS = buffer.readBigUInt64BE(offset + 20);
   }
+  
+  return {
+    version,
+    referenceID,
+    timescale,
+    earliestPTS: earliestPTS.toString()
+  };
 }
 
 /**
- * 解析ftyp box
+ * Parse ftyp box
  * @param {Buffer} buffer - 数据buffer
  * @param {number} offset - 起始位置
  * @param {number} size - box大小
+ * @returns {Object} ftyp box信息
  */
 function parseFtypBox(buffer, offset, size) {
   const majorBrand = buffer.toString('utf8', offset + 8, offset + 12);
@@ -82,98 +77,132 @@ function parseFtypBox(buffer, offset, size) {
     compatibleBrands.push(buffer.toString('utf8', offset + i, offset + i + 4));
   }
   
-  console.log('\n=== File Type Box (ftyp) ===');
-  console.log('Major Brand:', majorBrand);
-  console.log('Minor Version:', minorVersion);
-  console.log('Compatible Brands:', compatibleBrands.join(', '));
+  return {
+    majorBrand,
+    minorVersion,
+    compatibleBrands
+  };
 }
 
 /**
- * 解析moof box的基本信息
+ * Parse moof box
  * @param {Buffer} buffer - 数据buffer
  * @param {number} offset - 起始位置
  * @param {number} size - box大小
+ * @returns {Object} moof box信息
  */
 function parseMoofBox(buffer, offset, size) {
-  console.log('\n=== Movie Fragment Box (moof) ===');
-  console.log('Size:', size);
-  
-  // 解析mfhd (movie fragment header)
+  let sequenceNumber = null;
   let currentOffset = offset + 8;
+  
   while (currentOffset < offset + size) {
     const header = parseBoxHeader(buffer, currentOffset);
     if (header.type === 'mfhd') {
-      const sequenceNumber = buffer.readUInt32BE(currentOffset + 12);
-      console.log('Fragment Sequence Number:', sequenceNumber);
+      sequenceNumber = buffer.readUInt32BE(currentOffset + 12);
+      break;
     }
     currentOffset += header.size;
   }
+  
+  return {
+    size,
+    sequenceNumber
+  };
 }
 
 /**
- * 解析moov box的基本信息
+ * Parse moov box
  * @param {Buffer} buffer - 数据buffer
  * @param {number} offset - 起始位置
  * @param {number} size - box大小
+ * @returns {Object} moov box信息
  */
 function parseMoovBox(buffer, offset, size) {
-  console.log('\n=== Movie Box (moov) ===');
-  console.log('Size:', size);
-  
-  // 遍历子box
+  const subBoxes = [];
   let currentOffset = offset + 8;
+  
   while (currentOffset < offset + size) {
     const header = parseBoxHeader(buffer, currentOffset);
-    console.log('Found sub-box:', header.type);
+    subBoxes.push(header.type);
     currentOffset += header.size;
   }
+  
+  return {
+    size,
+    subBoxes
+  };
 }
 
 /**
- * 解析m4s文件
- * @param {string} filePath - 文件路径
+ * Parse M4S file and return box information
+ * @param {Buffer} buffer - M4S文件buffer
+ * @returns {Object} 解析结果
  */
-function parseM4S(filePath) {
-  console.log(`\nParsing file: ${filePath}`);
-  console.log('='.repeat(50));
+function parseM4S(buffer) {
+  const info = {
+    boxes: [],
+    moofSequence: null,
+    details: {}
+  };
   
-  const buffer = fs.readFileSync(filePath);
   let offset = 0;
-  
   while (offset < buffer.length) {
     const header = parseBoxHeader(buffer, offset);
+    info.boxes.push({
+      type: header.type,
+      size: header.size
+    });
     
     switch (header.type) {
       case 'ftyp':
-        parseFtypBox(buffer, offset, header.size);
+        info.details.ftyp = parseFtypBox(buffer, offset, header.size);
         break;
       case 'styp':
-        parseStypBox(buffer, offset, header.size);
+        info.details.styp = parseStypBox(buffer, offset, header.size);
         break;
       case 'sidx':
-        parseSidxBox(buffer, offset, header.size);
+        info.details.sidx = parseSidxBox(buffer, offset, header.size);
         break;
       case 'moov':
-        parseMoovBox(buffer, offset, header.size);
+        info.details.moov = parseMoovBox(buffer, offset, header.size);
         break;
       case 'moof':
-        parseMoofBox(buffer, offset, header.size);
+        const moofInfo = parseMoofBox(buffer, offset, header.size);
+        info.moofSequence = moofInfo.sequenceNumber;
+        info.details.moof = moofInfo;
         break;
       case 'mdat':
-        console.log('\n=== Media Data Box (mdat) ===');
-        console.log('Size:', header.size);
-        console.log('Contains encoded media data (audio/video)');
+        info.details.mdat = { size: header.size };
         break;
-      default:
-        console.log(`\n=== Unknown Box (${header.type}) ===`);
-        console.log('Size:', header.size);
     }
     
     offset += header.size;
   }
+  
+  return info;
 }
 
-// 创建表格行
+/**
+ * Parse path to extract track ID and target sequence
+ * @param {string} path - M4S文件路径
+ * @returns {Object} 解析结果
+ */
+function parseM4sPath(path) {
+  const match = path.match(/(\d+)-(\d+)\.m4s$/);
+  if (!match) {
+    throw new Error('Invalid m4s path format');
+  }
+  return {
+    trackId: parseInt(match[1], 10),
+    targetSequence: parseInt(match[2], 10)
+  };
+}
+
+/**
+ * Create table row for M4S analysis
+ * @param {Object} data - M4S文件分析数据
+ * @returns {string} 表格行
+ */
 function createTableRow(data) {
   const columns = [
     data.filename.padEnd(30),
@@ -190,101 +219,14 @@ function createTableRow(data) {
   return columns.join(' | ');
 }
 
-// 主函数
-function main() {
-  const m4sDir = path.join(process.cwd(), 'AD001');
-  
-  try {
-    const files = fs.readdirSync(m4sDir)
-      .filter(file => file.endsWith('.m4s'))
-      .map(file => path.join(m4sDir, file));
-    
-    if (files.length === 0) {
-      console.log('No m4s files found in the m4s directory');
-      return;
-    }
-
-    // 打印表头
-    console.log('\nM4S File Analysis Table');
-    console.log('='.repeat(120));
-    console.log([
-      'Filename'.padEnd(30),
-      'STYP Brand'.padEnd(10),
-      'STYP Ver'.padEnd(8),
-      'SIDX Ver'.padEnd(8),
-      'SIDX ID'.padEnd(8),
-      'Timescale'.padEnd(10),
-      'Earliest PTS'.padEnd(15),
-      'MOOF Sz'.padEnd(8),
-      'MOOF Seq'.padEnd(10),
-      'MDAT Sz'.padEnd(8)
-    ].join(' | '));
-    console.log('-'.repeat(120));
-
-    // 解析每个文件并收集数据
-    files.forEach(filePath => {
-      const buffer = fs.readFileSync(filePath);
-      let offset = 0;
-      const data = {
-        filename: path.basename(filePath),
-        stypMajorBrand: '',
-        stypMinorVersion: 0,
-        sidxVersion: 0,
-        sidxReferenceID: 0,
-        sidxTimescale: 0,
-        sidxEarliestPTS: 0,
-        moofSize: 0,
-        moofSequence: 0,
-        mdatSize: 0
-      };
-
-      while (offset < buffer.length) {
-        const header = parseBoxHeader(buffer, offset);
-        
-        switch (header.type) {
-          case 'styp':
-            data.stypMajorBrand = buffer.toString('utf8', offset + 8, offset + 12);
-            data.stypMinorVersion = buffer.readUInt32BE(offset + 12);
-            break;
-          case 'sidx':
-            data.sidxVersion = buffer.readUInt8(offset + 8);
-            data.sidxReferenceID = buffer.readUInt32BE(offset + 12);
-            data.sidxTimescale = buffer.readUInt32BE(offset + 16);
-            if (data.sidxVersion === 0) {
-              data.sidxEarliestPTS = buffer.readUInt32BE(offset + 20);
-            } else {
-              data.sidxEarliestPTS = buffer.readBigUInt64BE(offset + 20);
-            }
-            break;
-          case 'moof':
-            data.moofSize = header.size;
-            let currentOffset = offset + 8;
-            while (currentOffset < offset + header.size) {
-              const subHeader = parseBoxHeader(buffer, currentOffset);
-              if (subHeader.type === 'mfhd') {
-                data.moofSequence = buffer.readUInt32BE(currentOffset + 12);
-              }
-              currentOffset += subHeader.size;
-            }
-            break;
-          case 'mdat':
-            data.mdatSize = header.size;
-            break;
-        }
-        
-        offset += header.size;
-      }
-
-      console.log(createTableRow(data));
-    });
-    
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      console.error('m4s directory not found');
-    } else {
-      console.error('Error parsing m4s files:', error);
-    }
-  }
-}
-
-main();
+module.exports = {
+  parseBoxHeader,
+  parseStypBox,
+  parseSidxBox,
+  parseFtypBox,
+  parseMoofBox,
+  parseMoovBox,
+  parseM4S,
+  parseM4sPath,
+  createTableRow
+};
