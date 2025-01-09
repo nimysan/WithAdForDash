c6aee53ce8602fb05658382a6eb6e855054657ed
nimysan <234aini@gmail.com>
2025-01-09 17:12:09 +0800

    add sn in path

--- ad-m4s-function/src/server.js
+++ ad-m4s-function/src/server.js
@@ -43,7 +43,7 @@ app.get('*.m4s', async (req, res) => {
     // Read original m4s file from local path
     const offset = targetSequence%10;
     console.log("offset === "  + offset)
-    const originalM4sPath = path.join(__dirname, "..", "assets", "AD001", trackId+'-'+(38304768+offset)+'.m4s');
+    const originalM4sPath = path.join(__dirname, "..", "assets", sn, trackId+'-'+(38304768+offset)+'.m4s');
     console.log('Reading from:', originalM4sPath);
     const m4sData = fs.readFileSync(originalM4sPath);
     
