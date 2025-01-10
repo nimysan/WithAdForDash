2f137e281de4eea3adc59ba6c96a95ab512012d4
nimysan <234aini@gmail.com>
2025-01-10 14:13:15 +0800

    disable watch

--- ad-m4s-function/ecosystem.config.js
+++ ad-m4s-function/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'src/server.js',
     instances: 'max',     // Use maximum number of CPU cores
     exec_mode: 'cluster', // Run in cluster mode
-    watch: true,         // Auto restart on file changes
+    watch: false,        // Disable auto restart on file changes
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
